d889e6d3c265f4137076d56ced0e9326997de1f6
Sabrina R <47482751+expresso-patronum@users.noreply.github.com>
2022-04-10 23:27:18 -0300

    Update users-controllers.js

--- src/controllers/users-controllers.js
+++ src/controllers/users-controllers.js
@@ -10,15 +10,7 @@ const admins = [
 ];
 
 const { nanoid } = require("nanoid");
-/*
-const path= require('path');
-const fs= require('fs');
 
-const filePathAdmins = path.join(__dirname,  'admins.json' );
-const filePathUsers = path.join(__dirname,  'users.json' );
-
-const users= fs.readFileSync(filePathUsers,'utf8');
-const admins=fs.readFileSync(filePathAdmins,'utf8');*/
 
 const path= require('path');
 const fs= require('fs');
@@ -58,63 +50,34 @@ class UsersController {
         }
       );
     }
-    console.log({ users });
-    console.log({ admins });
+
     res.redirect("/login.html");
   }
 
   async detalhar(req, res) {
-    /*
-        const user = req.body;
-        const {
-            id
-        } = req.params;
-        let index;
-
-        if(user.tipo = 'user') {
-
-        for(let i=0; i< users .length;i++){
-            
-       if( users [i].id== id){
-           index= i;
-       }
-        }
-        res.render('detalharUsuario', {user: users[index]});
-    } if(user.tipo = 'admin') {
-        for(let i=0; i< admins .length;i++){
-            
-            if( admins [i].id== id){
-                index= i;
-            }
-             }
-            
-     
-             res.render('detalharUsuario', {user: admins[index]});
-    }
-    */
     res.render("detalharUsuario", { user: req.session.user });
   }
 
   async login(req, res) {
-    // ACHAR COM O EMAIL CERTO
+  
     const { email, senha, tipo } = req.body;
     let usuarioEncontrado;
     if(tipo== "user"|| tipo=="admin"){
     if (tipo == "user") {
         const filePathUsers = path.join(__dirname, "users.json");
         const users = JSON.parse(fs.readFileSync(filePathUsers, "utf8"));
-      console.log("oi user");
+    
       usuarioEncontrado = users.find((u) => u.email == email);
     } else {
         const filePathAdmins = path.join(__dirname, "admins.json");
         const admins = JSON.parse(fs.readFileSync(filePathAdmins, "utf8"));
-      console.log("oi adm");
+    
       usuarioEncontrado = admins.find((u) => u.email == email);
     }
   
     if (!usuarioEncontrado) return res.send("Usuário não encontrado");
     console.log(usuarioEncontrado);
-        // VERIFICAR A SENHA
+        
         if (usuarioEncontrado.senha == senha) {
           req.session.user = usuarioEncontrado;
           res.redirect("/posts");
@@ -128,7 +91,7 @@ class UsersController {
   }
 
   async logout(req, res) {
-    console.log("Passei por aqui...");
+ 
     req.session.destroy();
     return res.redirect('/posts');
 }
